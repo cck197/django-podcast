c77552582ae113344fd34f22a9de67583b42adf1
Christopher Kelly <chris@nourishbalancethrive.com>
2020-10-31 01:00:12 -0700

    add RFC 5005 pagination

--- podcast/views.py
+++ podcast/views.py
@@ -1,4 +1,5 @@
 from django.views.generic import DetailView, ListView
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 from .models import Enclosure, Episode, Show
 
@@ -8,12 +9,21 @@ class EpisodeDetail(DetailView):
     slug_url_kwarg = "episode_slug"
 
     def get_queryset(self):
-        return self.model.objects.published().filter(show__slug__exact=self.kwargs['show_slug'])
+        return self.model.objects.published().filter(
+            show__slug__exact=self.kwargs["show_slug"]
+        )
 
     def get_context_data(self, **kwargs):
         context = super(EpisodeDetail, self).get_context_data(**kwargs)
-        context['enclosure_list'] = Enclosure.objects.filter(episode__show__slug__exact=self.kwargs['show_slug']).filter(episode=self.object).order_by("-episode__date")
+        context["enclosure_list"] = (
+            Enclosure.objects.filter(
+                episode__show__slug__exact=self.kwargs["show_slug"]
+            )
+            .filter(episode=self.object)
+            .order_by("-episode__date")
+        )
         return context
+
 
 episode_detail = EpisodeDetail.as_view()
 
@@ -23,19 +33,27 @@ class EpisodeList(ListView):
     paginate_by = 12
 
     def get_queryset(self):
-        return self.model.objects.published().filter(show__slug__exact=self.kwargs['slug']).order_by('-date')
+        return (
+            self.model.objects.published()
+            .filter(show__slug__exact=self.kwargs["slug"])
+            .order_by("-date")
+        )
+
 
 episode_list = EpisodeList.as_view()
 
 
 class EpisodeSitemap(EpisodeList):
-    template_name = 'podcast/episode_sitemap.html'
-    content_type = 'application/xml'
+    template_name = "podcast/episode_sitemap.html"
+    content_type = "application/xml"
 
     def get_context_data(self, **kwargs):
         context = super(EpisodeDetail, self).get_context_data(**kwargs)
-        context['enclosure_list'] = Enclosure.objects.filter(episode__show__slug__exact=self.kwargs['slug']).order_by("-episode__date")
+        context["enclosure_list"] = Enclosure.objects.filter(
+            episode__show__slug__exact=self.kwargs["slug"]
+        ).order_by("-episode__date")
         return context
+
 
 episode_sitemap = EpisodeSitemap.as_view()
 
@@ -46,27 +64,49 @@ class ShowList(ListView):
 
     def get_queryset(self):
         if self.kwargs.get("slug"):
-            return self.model.objects.filter(slug__exact=self.kwargs['slug'])
+            return self.model.objects.filter(slug__exact=self.kwargs["slug"])
         return self.model.objects.all()
+
 
 show_list = ShowList.as_view()
 
 
 class ShowListAtom(DetailView):
     model = Show
-    template_name = 'podcast/show_feed_atom.html'
-    content_type = 'application/rss+xml'
+    template_name = "podcast/show_feed_atom.html"
+    content_type = "application/rss+xml"
+    paginate_by = 10
+
+    def get_context_data(self, **kwargs):
+        context = super(ShowListAtom, self).get_context_data(**kwargs)
+        episodes = self.object.episode_set.published()
+        paginator = Paginator(episodes, self.paginate_by)
+
+        page = self.request.GET.get("page")
+
+        try:
+            episodes = paginator.page(page)
+        except PageNotAnInteger:
+            episodes = paginator.page(1)
+        except EmptyPage:
+            episodes = paginator.page(paginator.num_pages)
+
+        context["episodes"] = episodes
+        return context
+
 
 show_list_atom = ShowListAtom.as_view()
 
 
 class ShowListFeed(ShowListAtom):
-    template_name = 'podcast/show_feed.html'
+    template_name = "podcast/show_feed.html"
+
 
 show_list_feed = ShowListFeed.as_view()
 
 
 class ShowListMedia(ShowListAtom):
-    template_name = 'podcast/show_feed_media.html'
+    template_name = "podcast/show_feed_media.html"
+
 
 show_list_media = ShowListMedia.as_view()
--- setup.py
+++ setup.py
@@ -1,26 +1,26 @@
 from setuptools import setup, find_packages
 
 template_patterns = [
-    'templates/*.html',
-    'templates/*/*.html',
+    "templates/*.html",
+    "templates/*/*.html",
 ]
 
-packages = find_packages('.')
+packages = find_packages(".")
 
 setup(
-    name='django-podcast',
-    version='0.5.4',
-    description='Django podcast app',
+    name="django-podcast",
+    version="0.5.5",
+    description="Django podcast app",
     packages=packages,
     package_data=dict((package_name, template_patterns) for package_name in packages),
-    url='https://github.com/jefftriplett/django-podcast',
+    url="https://github.com/cck197/django-podcast",
     classifiers=[
-        'Development Status :: 4 - Beta',
-        'Environment :: Web Environment',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Framework :: Django',
+        "Development Status :: 4 - Beta",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Framework :: Django",
     ],
 )
